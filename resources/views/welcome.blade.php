f68164d3ca1580cd45a2a7524aedd8a21af60274
JonathanLeal <jonathan.e.leal.m@gmail.com>
2023-05-26 14:39:16 -0600

    Agregando estilos a pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,11 +9,64 @@
     <title>Inicio</title>
 </head>
 <body>
-    
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">INICIO</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sociedades
+                </button>
+                <ul class="dropdown-menu dropdown-menu-primary">
+                  <li><a class="dropdown-item" href="SocJovenes">Jovenes</a></li>
+                  <li><a class="dropdown-item" href="SocSenioras">Señoras</a></li>
+                  <li><a class="dropdown-item" href="socSeniores">Señores</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Privilegios
+                </button>
+                <ul class="dropdown-menu dropdown-menu-primary">
+                  <li><a class="dropdown-item" href="SocJovenes">Jovenes</a></li>
+                  <li><a class="dropdown-item" href="SocSenioras">Señoras</a></li>
+                  <li><a class="dropdown-item" href="socSeniores">Señores</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Miembros
+                </button>
+                <ul class="dropdown-menu dropdown-menu-primary">
+                  <li><a class="dropdown-item" href="SocJovenes">Jovenes</a></li>
+                  <li><a class="dropdown-item" href="SocSenioras">Señoras</a></li>
+                  <li><a class="dropdown-item" href="socSeniores">Señores</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    <div class="container mt-3">
+        <div class="card">
+            <div class="card-header">
+                <h1>Este es un espacio para la gloria de Dios</h1>
+                <div class="card-body">
+                    <h1>Iglesia Apostolica de la fe en Cristo Jesus Monte Tabor</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.0/dist/sweetalert2.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="{{asset('js/SocJovenes.js')}}"></script>
 </body>
 </html>
